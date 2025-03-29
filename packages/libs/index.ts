3d46b6f655f385217938c1d2260fd7763c304471
Axel Hamilcaro <axel.hamilcaro-pro@outlook.fr>
2025-03-29 13:42:27 +0100

    feat: refactor API structure and add HTTP utility classes

--- packages/libs/index.ts
+++ packages/libs/index.ts
@@ -5,6 +5,9 @@ export { default as slugify } from "slugify";
 export { v4 as uuidv4 } from "uuid";
 export { z } from "zod";
 
+export * from "./utils/http/httpCode.enum";
+export * from "./utils/http/HttpException";
+export * from "./utils/http/httpResponse";
 export * from "./utils/number";
 export * from "./utils/slug";
 export * from "./utils/validation";
